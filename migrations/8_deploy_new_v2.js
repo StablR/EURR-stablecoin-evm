70f031026ca8e42da119c46c19fcc461f34d40f1
Venimir Petkov <venimirp@gmail.com>
2022-02-23 13:42:12 +0100

    uncomment test addresses in migration

--- migrations/8_deploy_new_v2.js
+++ migrations/8_deploy_new_v2.js
@@ -29,11 +29,11 @@ module.exports = async (deployer, network) => {
     // DO NOT USE THESE ADDRESSES IN PRODUCTION - these are the deterministic
     // addresses from ganache, so the private keys are well known and match the
     // values we use in the tests
-    // proxyAdminAddress = "0x2F560290FEF1B3Ada194b6aA9c40aa71f8e95598";
-    // ownerAddress = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
-    // masterMinterAddress = "0x3E5e9111Ae8eB78Fe1CC3bb8915d5D461F3Ef9A9";
-    // pauserAddress = "0xACa94ef8bD5ffEE41947b4585a84BdA5a3d3DA6E";
-    // blacklisterAddress = "0xd03ea8624C8C5987235048901fB614fDcA89b117";
+    proxyAdminAddress = "0x2F560290FEF1B3Ada194b6aA9c40aa71f8e95598";
+    ownerAddress = "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d";
+    masterMinterAddress = "0x3E5e9111Ae8eB78Fe1CC3bb8915d5D461F3Ef9A9";
+    pauserAddress = "0xACa94ef8bD5ffEE41947b4585a84BdA5a3d3DA6E";
+    blacklisterAddress = "0xd03ea8624C8C5987235048901fB614fDcA89b117";
   }
 
   console.log(`Proxy Admin:   ${proxyAdminAddress}`);
